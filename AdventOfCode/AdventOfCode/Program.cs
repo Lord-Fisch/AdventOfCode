079c1b2fa660b739959c53f799d2733149276a5c
Tim van den Berg <vandenBerg91@gmail.com>
2017-12-01 11:45:37 +0100

    Day1

---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("Day 1 Part 1: {0}", Day1.Part1()));
+
+            Console.ReadKey();
+        }
+    }
+}
